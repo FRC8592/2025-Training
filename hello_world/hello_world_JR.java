043e1cbc2c18e8a6dcf4e527190a922b717cc6d8
Jack Runski <frc8592-jackr@novalabsrobotics.org>
2025-09-14 20:36:49 -0400

    Wrote a basic print function

---
+++ hello_world/hello_world_JR.java
@@ -0,0 +1,7 @@
+class hello_world_JR {
+    public static void main (String[] args) {
+        System.out.println("AHHHHHHHH");
+        System.out.println("Screaming brought to you by Jack");
+    }
+
+}
