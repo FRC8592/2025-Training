85ec6197aea0d97825312867753b76dcc707eaec
staubs999 <tomstauber@gmail.com>
2025-09-17 15:52:11 -0400

    TASs Hello 3 Code

---
+++ hello_world/Hello3_TAS.java
@@ -0,0 +1,10 @@
+
+class Hello3_TAS {
+    public static void main(String[] args) {
+        //Program execution begins here 
+        String foo = "IAP 6.092";
+        System.out.println(foo); // Print "IAP 6.092"
+        foo = "Coach Joe is my hero!!!!!";
+        System.out.println(foo); // You know it!!
+    }
+}
