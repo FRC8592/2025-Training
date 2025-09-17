97856f3a4590119b0369468153ec5596cd078eb5
staubs999 <tomstauber@gmail.com>
2025-09-17 15:51:35 -0400

    TASs Hello2 code

---
+++ hello_world/Hello2_TAS.java
@@ -0,0 +1,8 @@
+
+class Hello2_TAS {
+    public static void main(String[] args) {
+        //Program execution begins here 
+        System.out.println("Hello crazy world"); // Print Once
+        System.out.println("Line number 2"); // Again!!
+    }
+}
