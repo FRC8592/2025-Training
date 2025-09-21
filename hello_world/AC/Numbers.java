d8b87fb7f7a8f6bf4090cc5b74f38532caea5322
kii2739 <annieyuhanchen@gmail.com>
2025-09-21 18:10:20 -0400

    added more java training

---
+++ hello_world/AC/Numbers.java
@@ -0,0 +1,11 @@
+package AC;
+
+public class Numbers {
+    public static void main(String[] args)
+    {
+        double d = (double)5/(double)2;
+        System.out.println(d);
+        d = 5.0/2.0;
+        System.out.println(d);
+    }
+}
