b62acc32ccdb2840d7ff26d5781d240b1ad2f831
kii2739 <annieyuhanchen@gmail.com>
2025-10-12 21:40:52 -0400

    added homework for java training

--- hello_world/AC/FooCorporation.java
+++ hello_world/AC/FooCorporation.java
@@ -1,5 +1,36 @@
 package AC;
-
+import java.util.Scanner;
 public class FooCorporation {
-    
+
+    public static double pay(double work)
+    {
+        if (work > 40)
+        {
+            if (work > 60)
+            {
+                double answer = 580;
+                return answer;
+            }
+            else 
+            {
+                double answer = 320 + 12 * (work - 40);
+                return answer;
+            }
+        } 
+        else
+        {
+            double answer = work * 8;
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner tetoscanner = new Scanner (System.in);
+        System.out.println("how many hours did you work?");
+
+        double work = tetoscanner.nextDouble();
+
+        double answer = pay(work);
+
+        System.out.println("you will recieve $" + answer + ".");
+    }
 }
