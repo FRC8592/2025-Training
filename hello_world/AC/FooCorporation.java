b8ce3c5f639017fe2355b738e2e2bd1c2e8f07cc
kii2739 <annieyuhanchen@gmail.com>
2025-10-26 16:50:48 -0400

    added onto java training

--- hello_world/AC/FooCorporation.java
+++ hello_world/AC/FooCorporation.java
@@ -2,35 +2,45 @@ package AC;
 import java.util.Scanner;
 public class FooCorporation {
 
-    public static double pay(double work)
+    public static double pay(double work, double basepay)
     {
         if (work > 40)
         {
-            if (work > 60)
+            if (work >= 60)
             {
-                double answer = 580;
+                double answer = basepay * 40 + 1.5 * basepay * 20;
+                System.out.println("remember, there's a maximum of 20 hours of overtime!");
                 return answer;
             }
             else 
             {
-                double answer = 320 + 12 * (work - 40);
+                double answer = basepay * 40 + 1.5 * basepay * (work - 40);
                 return answer;
             }
         } 
         else
         {
-            double answer = work * 8;
+            double answer = work * basepay;
             return answer;
         }
     }
     public static void main(String[] args) {
         Scanner tetoscanner = new Scanner (System.in);
         System.out.println("how many hours did you work?");
-
         double work = tetoscanner.nextDouble();
 
-        double answer = pay(work);
+        System.out.println("what's the base pay per hour?");
+        double basepay = tetoscanner.nextDouble();
 
-        System.out.println("you will recieve $" + answer + ".");
+        if (basepay >= 8)
+        {
+            double answer = pay(work, basepay);
+
+            System.out.println("you will recieve $" + answer + ".");
+        }
+        else
+        {
+            System.out.println("base pay must be at least 8 dollars :(");
+        }
     }
 }
